eedaa1870cbd8df07511b7ccd6659cab63bf9afb
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-07 03:28:15 +0000

    initiated BCRYPT wut wutgit add .

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
@@ -13,6 +14,31 @@ app.use(cookieParser());
 app.set("view engine", "ejs");
 
 // function for generating random URL
+
+
+// 'Database'
+
+const urlDatabase = {
+  "userRandomID": {
+    "b2xVn2": "http://www.lighthouselabs.ca"
+  },
+  "user2RandomID": {
+    "9sm5xK": "http://www.google.com"
+  }
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    hashedPassword: "$2a$10$egEsYw8OXbvy9i3AU9Bsu.ukOHRCF8k66jLEz7y5MW5Xn5gm9kfAG"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    hashedPassword: "$2a$10$rVbXkED9fM5V9kolvAERAObnvFFt6XncNyr//XnedxnAL79.S1RKW"
+  }
+};
 
 function generateRandomString() {
   const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -24,44 +50,20 @@ function generateRandomString() {
 }
 
 function verifyUser(email, password) {
-    for (let user in users) {
+  for (let user in users) {
     let currentUser = users[user];
-      if (currentUser['email'] === email && currentUser['password'] === password) {
-          return currentUser;
-      }
+    if (currentUser['email'] === email && bcrypt.compareSync(password, currentUser['hashedPassword'])) {
+      return currentUser;
+    }
   }
 }
 
 function cookieCheck(reqCookies) {
-  if (reqCookies['user_id'] === urlDatabase[reqCookies['user_id']])
-    return true;
-}
-// 'Database'
-
-const urlDatabase = {
-  "userRandomID": {
-      "b2xVn2": "http://www.lighthouselabs.ca"
-  },
-  "user2RandomID": {
-      "9sm5xK": "http://www.google.com"
-   }
-};
-
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
+  if (reqCookies['user_id'] === urlDatabase[reqCookies['user_id']]) { return true; }
 }
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.redirect("/login");
 });
 
 // GET login request
@@ -120,7 +122,7 @@ app.post("/urls", (req, res) => {
   if (re.test(longy)) {
     return urlDatabase[req.cookies["user_id"]][shorty] = longy;
   } else {
-    return urlDatabase[req.cookies["user_id"]][shorty] = `https://${longy}`;
+    urlDatabase[req.cookies["user_id"]][shorty] = `https://${longy}`;
   }
   res.redirect(`/urls/${shorty}`);
 });
@@ -128,14 +130,14 @@ app.post("/urls", (req, res) => {
 // POST request to delete URL
 app.post("/urls/:id/delete", (req, res) => {
   if (cookieCheck(req.cookies['user_id'])) {
-   delete urlDatabase[req.cookies['user_id']][req.params.id]
+    delete urlDatabase[req.cookies['user_id']][req.params.id];
   } res.redirect('/urls');
 });
 
 //POST request for editing longURL
 app.post("/urls/:id", (req, res) => {
   if (cookieCheck(req.cookies['user_id'])) {
-  urlDatabase[req.cookies['user_id']][req.params.id] = req.body.longURL;
+    urlDatabase[req.cookies['user_id']][req.params.id] = req.body.longURL;
   }  res.redirect("/urls");
 
 });
@@ -144,8 +146,8 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   currentUser = verifyUser(req.body.email, req.body.password);
   if (currentUser === undefined) {
-   return res.status(400).send('Bad Request');
-  } else return res.cookie('user_id', currentUser['id']);
+    return res.status(400).send('Bad Request');
+  } else { res.cookie('user_id', currentUser['id']); }
 
 
   res.redirect("/urls");
@@ -160,21 +162,22 @@ app.post("/logout", (req, res) => {
 // POST request with registration data
 app.post("/register", (req, res) => {
   if (req.body.email && req.body.password === "") {
-   return res.status(400).send('Bad Request');
+    return res.status(400).send('Bad Request');
 
   }
   for (let user in users) {
     const uemail = users[user];
-      if (uemail['email'] === req.body.email) {
-          return res.status(400).send('Email already in use');
-      }
+    if (uemail['email'] === req.body.email) {
+      return res.status(400).send('Email already in use');
+    }
   }
   const randomID = generateRandomString();
   users[randomID] = {
     id: randomID,
     email: req.body.email,
-    password: req.body.password
+    hashedPassword: bcrypt.hashSync(req.body.password, 10)
   };
+  console.log(users[randomID].hashedPassword);
   res.cookie('user_id', randomID);
   res.redirect('/urls');
 });
@@ -184,11 +187,11 @@ app.get("/u/:shortURL", (req, res) => {
   let templateVars = {
     user: (users[req.cookies["user_id"]])
   };
- let longURL = "";
+  let longURL = "";
   for (let user in urlDatabase) {
     let currentUser = urlDatabase[user];
     if (currentUser.hasOwnProperty(req.params.shortURL)) {
-     return longURL = longURL + currentUser[req.params.shortURL];
+      return longURL = longURL + currentUser[req.params.shortURL];
     }
   }
   res.redirect(longURL);
